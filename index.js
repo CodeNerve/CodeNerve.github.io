16eb1a145c0e99b266ea29eddfd7537059e17dda
Arslanbek Zhaparov <arslanbekzhaparov@gmail.com>
2022-11-14 14:25:14 -0800

    library

--- index.js
+++ index.js
@@ -1,109 +1,70 @@
-var Typer = {
-  text: '',
-  accessCountimer: null,
-  index: 0,
-  speed: 2,
-  file: '',
-  accessCount: 0,
-  deniedCount: 0,
-  init: function () {
-    accessCountimer = setInterval(function () {
-      Typer.updLstChr();
-    }, 500);
-    $.get(Typer.file, function (data) {
-      Typer.text = data;
-      Typer.text = Typer.text.slice(0, Typer.text.length - 1);
-    });
+$(function() {
+  var data = [
+  { 
+    action: 'type',
+    strings: ["npm install -g mimik^400"],
+    output: '<span class="gray">+mimik@0.10.2 installed</span><br>&nbsp;',
+    postDelay: 1000
   },
-
-  content: function () {
-    return $('#console').html();
+  { 
+    action: 'type',
+    strings: ["cd tests^400"],
+    output: ' ',
+    postDelay: 1000
   },
-
-  write: function (str) {
-    $('#console').append(str);
-    return false;
+  { 
+    action: 'type',
+    //clear: true,
+    strings: ['mimik run^400'],
+    output: $('.mimik-run-output').html()
   },
-
-  addText: function (key) {
-    if (key.keyCode == 18) {
-      Typer.accessCount++;
-
-      if (Typer.accessCount >= 3) {
-        Typer.makeAccess();
-      }
-    } else if (key.keyCode == 20) {
-      Typer.deniedCount++;
-
-      if (Typer.deniedCount >= 3) {
-        Typer.makeDenied();
-      }
-    } else if (key.keyCode == 27) {
-      Typer.hidepop();
-    } else if (Typer.text) {
-      var cont = Typer.content();
-      if (cont.substring(cont.length - 1, cont.length) == '|')
-        $('#console').html(
-          $('#console')
-            .html()
-            .substring(0, cont.length - 1),
-        );
-      if (key.keyCode != 8) {
-        Typer.index += Typer.speed;
-      } else {
-        if (Typer.index > 0) Typer.index -= Typer.speed;
-      }
-      var text = Typer.text.substring(0, Typer.index);
-      var rtn = new RegExp('\n', 'g');
-
-      $('#console').html(text.replace(rtn, '<br/>'));
-      window.scrollBy(0, 50);
-    }
-
-    if (key.preventDefault && key.keyCode != 122) {
-      key.preventDefault();
-    }
-
-    if (key.keyCode != 122) {
-      // otherway prevent keys default behavior
-      key.returnValue = false;
-    }
-  },
-
-  updLstChr: function () {
-    var cont = this.content();
-
-    if (cont.substring(cont.length - 1, cont.length) == '|')
-      $('#console').html(
-        $('#console')
-          .html()
-          .substring(0, cont.length - 1),
-      );
-    else this.write('|'); // else write it
-  },
-};
-
-function replaceUrls(text) {
-  var http = text.indexOf('http://');
-  var space = text.indexOf('.me ', http);
-
-  if (space != -1) {
-    var url = text.slice(http, space - 1);
-    return text.replace(url, '<a href="' + url + '">' + url + '</a>');
-  } else {
-    return text;
+  { 
+    action: 'type',
+    strings: ["that was easy!", ''],
+    postDelay: 2000
   }
-}
+  
+];
+  runScripts(data, 0);
+});
 
-Typer.speed = 3;
-Typer.file = 'CodeNerve.txt';
-Typer.init();
+function runScripts(data, pos) {
+    var prompt = $('.prompt'),
+        script = data[pos];
+    if(script.clear === true) {
+      $('.history').html(''); 
+    }
+    switch(script.action) {
+        case 'type':
+          // cleanup for next execution
+          prompt.removeData();
+          $('.typed-cursor').text('');
+          prompt.typed({
+            strings: script.strings,
+            typeSpeed: 30,
+            callback: function() {
+              var history = $('.history').html();
+              history = history ? [history] : [];
+              history.push('$ ' + prompt.text());
+              if(script.output) {
+                history.push(script.output);
+                prompt.html('');
+                $('.history').html(history.join('<br>'));
+              }
+              // scroll to bottom of screen
+              $('section.terminal').scrollTop($('section.terminal').height());
+              // Run next script
+              pos++;
+              if(pos < data.length) {
+                setTimeout(function() {
+                  runScripts(data, pos);
+                }, script.postDelay || 1000);
+              }
+            }
+          });
+          break;
+        case 'view':
 
-var timer = setInterval('t();', 30);
-function t() {
-  Typer.addText({ keyCode: 123748 });
-
-  if (Typer.index > Typer.text.length) {
-    clearInterval(timer);
-  }
+          break;
+    }
 }
